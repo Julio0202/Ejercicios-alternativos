a19e10cc86f9fcd69247fdb6d0958b9c82edca2c
Julio Chen <al_ychen@iessierradelasvillas.org>
2022-10-17 11:15:16 +0200

    Cambio en el 9

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -1,6 +1,4 @@
 from operator import truediv
-
-
 print("Dime un número")
 num1 = int(input())
 print("Dime otro")
@@ -13,3 +11,7 @@ lista.append(num2)
 lista.append(num3)
 lista.sort(reverse=True)
 print(lista)
+
+print("El mayor es", lista[0])
+ultimo = len(lista)
+print("El menor es", lista[ultimo-1])
